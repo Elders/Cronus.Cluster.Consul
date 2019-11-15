0929b05f34ccba4ce2db23fb42da76b3eb8c0eca
mynkow <mynkow@gmail.com>
2019-11-15 14:47:40 +0200

    Configures consul with options pattern

---
+++ src/Elders.Cronus.Cluster.Consul/ConsulClusterOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace Elders.Cronus.Persistence.Cassandra
+{
+    public class ConsulClusterOptions
+    {
+        public string Address { get; set; }
+        public int Port { get; set; }
+    }
+}
---
+++ src/Elders.Cronus.Cluster.Consul/ConsulClusterOptionsProvider.cs
@@ -0,0 +1,16 @@
+﻿using Elders.Cronus.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace Elders.Cronus.Persistence.Cassandra
+{
+    public class ConsulClusterOptionsProvider : CronusOptionsProviderBase<ConsulClusterOptions>
+    {
+        public ConsulClusterOptionsProvider(IConfiguration configuration) : base(configuration) { }
+
+        public override void Configure(ConsulClusterOptions options)
+        {
+            options.Address = configuration["cronus:cluster:consul:address"];
+            options.Address = configuration["cronus:cluster:consul:port"];
+        }
+    }
+}
